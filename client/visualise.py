60eaef0940b4f41fbf1074bc8e0b3368fc25aecb
WKoishi <intelwzt@outlook.com>
2021-06-18 11:31:08 +0800

    优化代码结构

--- client/visualise.py
+++ client/visualise.py
@@ -9,7 +9,9 @@ from PyQt5.QtWidgets import QWidget
 from math import sin, cos, pi
 
 class ModelVisualise(QWidget):
-
+    """
+    倒立摆模型可视化
+    """
     def __init__(self):      
         super().__init__()
         
@@ -21,10 +23,9 @@ class ModelVisualise(QWidget):
         self.stick_length = 110
 
 
-    def set_value(self, position, theta):
+    def update_state(self, position, theta):
         self.position = position + int(self.MAX_VIEW / 2)
         self.theta = theta
-
         self.repaint()
 
     def paintEvent(self, event):
@@ -36,23 +37,37 @@ class ModelVisualise(QWidget):
         Qheight = size.height()
         #width_mid = int(Qwidth/2)
 
+        # 计算小车在绘画区域的位置坐标
         x_posi = int(((Qwidth-self.car_size[0]) / self.MAX_VIEW) * self.position)
         y_posi = Qheight - self.stick_length
         rotate_center = (int(x_posi+self.car_size[0]/2), y_posi)
 
         pen = QPen()
 
+        # 绘制摆杆
         pen.setWidth(12)
-        pen.setColor(QColor(0, 0, 179))
+        pen.setColor(QColor(114, 136, 208))
         qp.setPen(pen)
         end_point_x = rotate_center[0] - int(self.stick_length * sin(-self.theta))
         end_point_y = rotate_center[1] - int(self.stick_length * cos(-self.theta))
         qp.drawLine(rotate_center[0], rotate_center[1], end_point_x, end_point_y)
 
+        # 绘制小车
+
+        left_wheel_center = (x_posi + 15, y_posi + self.car_size[1])
+        right_wheel_center = (x_posi + 65, y_posi + self.car_size[1])
+
         pen.setWidth(2)
+        pen.setColor(QColor(20, 20, 20))
+        qp.setPen(pen)
+        qp.setBrush(QColor(0, 0, 200))
+        qp.drawEllipse(left_wheel_center[0] - 7, left_wheel_center[1] - 7, 14, 14)
+        qp.drawEllipse(right_wheel_center[0] - 7, right_wheel_center[1] - 7, 14, 14)
+
         pen.setColor(QColor(255, 255, 255))
         qp.setPen(pen)
         qp.setBrush(QColor(255, 255, 184))
+        qp.drawEllipse(rotate_center[0] - 9, rotate_center[1] - 9, 18, 18)
         qp.drawRect(x_posi, y_posi, self.car_size[0], self.car_size[1])
 
         qp.end()
