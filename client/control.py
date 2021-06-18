bfabb748a62d4952d03dd5be99d348ff58ca7dac
WKoishi <intelwzt@outlook.com>
2021-06-18 13:55:28 +0800

    完善代码注释

--- client/control.py
+++ client/control.py
@@ -10,7 +10,9 @@ from math import pi, cos
 
 
 class TotalController:
-
+    """
+    总控制器
+    """
     def __init__(self):
 
         self.sample_time = 0.014
@@ -25,6 +27,7 @@ class TotalController:
                                             output_upper_limit = 1000,
                                             sample_time = self.sample_time)
 
+        # PID/LQR切换开关
         self.LQR_CONTROL_ENABLE = 1
 
         self.now_position = 0
@@ -37,17 +40,25 @@ class TotalController:
         self.posi_nabla = 0
         self.theta_nabla = 0
 
+        # 通过LQR算法得出的状态反馈增益
         self.lqr_feedback_gain_fast = [-82.8127, -17.4970, -10.0, -12.0002]
         self.lqr_feedback_gain_slow = [-47.6602, -11.8337, -0.3162, -1.3513]
         self.is_slow_mode = False
 
         self.position_target = 0
 
+        # 是否已经起摆
         self.is_swing_up = False
         self.is_impulse = 1
 
-    def swing_up(self, gain, theta, theta_nabla):
-        
+    def swing_up_control(self, gain, theta, theta_nabla):
+        '''
+        能量起摆控制器
+
+        Return
+        ------------
+        力（force）输入
+        '''
         kinetic_energy = (1/240) * theta_nabla**2
 
         potential_energy = (cos(theta)-1) * 9.8 * 0.25 * 0.1
@@ -69,6 +80,9 @@ class TotalController:
         return output
 
     def lqr_control(self, target, theta, theta_nabla, posi, posi_nabla):
+        """
+        状态反馈LQR控制器
+        """
         if self.is_slow_mode:
             feedback_gain = self.lqr_feedback_gain_slow
         else:
@@ -85,6 +99,9 @@ class TotalController:
         return output
 
     def pid_control(self, target, theta, last_theta, posi, last_posi):
+        """
+        PID控制器
+        """
         position_gain = 0.3
         force_gain = -50
 
@@ -94,19 +111,22 @@ class TotalController:
         # 外环反馈通道PD控制器
         posi_pid_output = self.posi_PD_controller.pid_cal(posi)
 
+        # 外环前向通道增益
         theta_input = position_gain * (target - posi_pid_output)
 
         # 内环反馈通道PD控制器
         theta_pid_output = self.theta_PD_controller.pid_cal(theta)
 
-        # 得到被控对象的输入
+        # 得到被控对象的输入（内环前向通道增益）
         output = force_gain * (theta_input - theta_pid_output)
 
         return output
 
 
     def runtime(self, posi_target, now_position, now_theta):
-
+        """
+        控制器主流程
+        """
         # 获取当前位置并计算位置的微分
         self.last_position = self.now_position
         self.now_position = now_position
@@ -123,6 +143,7 @@ class TotalController:
             elif abs(self.now_theta) < pi/18 and self.is_slow_mode == True:
                 self.is_slow_mode = False
 
+        # 起摆时摆角小于30°即切换成稳摆控制
         if self.is_swing_up == False and abs(self.now_theta) < pi/6:
             self.is_swing_up = True
 
@@ -140,9 +161,9 @@ class TotalController:
 
         else:
 
-            force_input = self.swing_up(350, self.last_theta, self.theta_nabla)
+            force_input = self.swing_up_control(350, self.last_theta, self.theta_nabla)
 
-            # 产生一个冲激信号
+            # 开始起摆时产生一个单位冲激信号
             if self.is_impulse:
                 force_input = 1
                 self.is_impulse += 1
